239e057b788e71ff8a39d09dc50413837a54040b
AurelienRibon <AurelienRibon@localhost>
2011-08-28 19:38:45 +0200

    ! FIXED A GOD DAM DEMENTIAL BUG ! PLEASE SEND ME TO ASYLIUM FOR NOT HAVING SPOT IT SOONER !

--- editor/src/aurelienribon/box2deditor/utils/VectorUtils.java
+++ editor/src/aurelienribon/box2deditor/utils/VectorUtils.java
@@ -7,21 +7,24 @@ import com.badlogic.gdx.math.Vector2;
  * @author Aurelien Ribon (aurelien.ribon@gmail.com)
  */
 public class VectorUtils {
-	public static Vector2 mul(Vector2 v, float coeff) {
-		return new Vector2(v).mul(coeff);
-	}
-
-	public static Vector2[] mul(Vector2[] vs, float coeff) {
-		Vector2[] ret = new Vector2[vs.length];
-		for (int i=0; i<ret.length; i++)
-			ret[i] = mul(vs[i], coeff);
+	public static Vector2 mul(Vector2 v, float cx, float cy) {
+		Vector2 ret = v.cpy();
+		ret.x *= cx;
+		ret.y *= cy;
 		return ret;
 	}
 
-	public static Vector2[][] mul(Vector2[][] vss, float coeff) {
+	public static Vector2[] mul(Vector2[] vs, float cx, float cy) {
+		Vector2[] ret = new Vector2[vs.length];
+		for (int i=0; i<ret.length; i++)
+			ret[i] = mul(vs[i], cx, cy);
+		return ret;
+	}
+
+	public static Vector2[][] mul(Vector2[][] vss, float cx, float cy) {
 		Vector2[][] ret = new Vector2[vss.length][];
 		for (int i=0; i<ret.length; i++)
-			ret[i] = mul(vss[i], coeff);
+			ret[i] = mul(vss[i], cx, cy);
 		return ret;
 	}
 
