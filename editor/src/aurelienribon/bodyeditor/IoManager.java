3a650c90d447a25bd391aba7255c1053c522bcb2
AurelienRibon <AurelienRibon@localhost>
2011-09-29 17:47:23 +0200

    + added XML support!! Hurray!

--- editor/src/aurelienribon/bodyeditor/IoManager.java
+++ editor/src/aurelienribon/bodyeditor/IoManager.java
@@ -9,8 +9,10 @@ import aurelienribon.utils.notifications.ChangeableObject;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import com.sun.org.apache.xpath.internal.NodeSet;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -18,7 +20,20 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import org.apache.commons.io.FilenameUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -77,12 +92,16 @@ public class IoManager extends ChangeableObject {
 	}
 
 	// -------------------------------------------------------------------------
-	// Import/Export
+	// Export
 	// -------------------------------------------------------------------------
 
 	/**
 	 * Writes the current content of the AssetsManager to the output file.
-	 * Save is done like this:
+	 * If the file extension matches a registered extension, like xml, then it
+	 * is written according to the registered writer. Else, the binary writer
+	 * is used.<br/><br/>
+	 *
+	 * Binary format is as follows:
 	 * <pre>
 	 * for each asset:
 	 *     UTFString: relative path
@@ -104,7 +123,67 @@ public class IoManager extends ChangeableObject {
     public void exportToOutputFile() throws IOException {
 		if (outputFile == null)
 			throw new IOException("output file was not set");
-		
+
+		String ext = FilenameUtils.getExtension(outputFile.getName());
+		if (ext.equalsIgnoreCase("xml")) {
+			exportAsXml();
+		} else {
+			exportAsBinary();
+		}
+	}
+
+	private void exportAsXml() throws IOException {
+		Document doc = null;
+		try {
+			doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		} catch (ParserConfigurationException ex) {
+			return;
+		}
+
+		Element assetsElem = doc.createElement("Assets");
+		doc.appendChild(assetsElem);
+
+		for (AssetModel am : AssetsManager.instance().getList()) {
+			Vector2 normalizeCoeff = getNormalizeCoeff(am.getPath());
+
+			Element assetElem = doc.createElement("Asset");
+			assetElem.setAttribute("relativePath", relativize(am.getPath()));
+			assetsElem.appendChild(assetElem);
+
+			for (ShapeModel shape : am.getShapes()) {
+				Element shapeElem = doc.createElement("Shape");
+				assetElem.appendChild(shapeElem);
+				for (Vector2 vertex : shape.getVertices()) {
+					Element vertexElem = doc.createElement("Vertex");
+					vertexElem.setAttribute("x", Float.toString(vertex.x / normalizeCoeff.x));
+					vertexElem.setAttribute("y", Float.toString(vertex.y / normalizeCoeff.y));
+					shapeElem.appendChild(vertexElem);
+				}
+			}
+
+			for (PolygonModel polygon : am.getPolygons()) {
+				Element polygonElem = doc.createElement("Polygon");
+				assetElem.appendChild(polygonElem);
+				for (Vector2 vertex : polygon.getVertices()) {
+					Element vertexElem = doc.createElement("Vertex");
+					vertexElem.setAttribute("x", Float.toString(vertex.x / normalizeCoeff.x));
+					vertexElem.setAttribute("y", Float.toString(vertex.y / normalizeCoeff.y));
+					polygonElem.appendChild(vertexElem);
+				}
+			}
+		}
+
+		OutputFormat format = new OutputFormat(doc);
+		format.setLineWidth(0);
+		format.setIndenting(true);
+		format.setIndent(4);
+		FileOutputStream fos = new FileOutputStream(outputFile);
+		XMLSerializer serializer = new XMLSerializer(fos, format);
+		serializer.serialize(doc);
+		fos.close();
+	}
+
+	private void exportAsBinary() throws IOException {
 		DataOutputStream os = new DataOutputStream(new FileOutputStream(outputFile));
 
 		for (AssetModel am : AssetsManager.instance().getList()) {
@@ -114,16 +193,30 @@ public class IoManager extends ChangeableObject {
 			os.writeUTF(name);
 
 			os.writeInt(am.getShapes().size());
-			for (ShapeModel shape : am.getShapes())
-				writeVecs(os, shape.getVertices().toArray(new Vector2[0]), normalizeCoeff);
+			for (ShapeModel shape : am.getShapes()) {
+				os.writeInt(shape.getVertices().size());
+				for (Vector2 vertex : shape.getVertices()) {
+					os.writeFloat(vertex.x / normalizeCoeff.x);
+					os.writeFloat(vertex.y / normalizeCoeff.y);
+				}
+			}
 
 			os.writeInt(am.getPolygons().size());
-			for (PolygonModel polygon : am.getPolygons())
-				writeVecs(os, polygon.getVertices().toArray(new Vector2[0]), normalizeCoeff);
+			for (PolygonModel polygon : am.getPolygons()) {
+				os.writeInt(polygon.getVertices().size());
+				for (Vector2 vertex : polygon.getVertices()) {
+					os.writeFloat(vertex.x / normalizeCoeff.x);
+					os.writeFloat(vertex.y / normalizeCoeff.y);
+				}
+			}
 		}
 
 		os.close();
 	}
+
+	// -------------------------------------------------------------------------
+	// Import
+	// -------------------------------------------------------------------------
 
 	/**
 	 * Reads the output file and update the AssetsManager with its content.
@@ -136,20 +229,91 @@ public class IoManager extends ChangeableObject {
 			throw new IOException("output file was not set");
 
 		AssetsManager.instance().getList().clear();
+		String ext = FilenameUtils.getExtension(outputFile.getName());
+		if (ext.equalsIgnoreCase("xml")) {
+			importAsXml();
+		} else {
+			importAsBinary();
+		}
+	}
+
+	private void importAsXml() throws IOException {
+		try {
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(outputFile);
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			NodeList assetsNodes = (NodeList)xpath.evaluate("/Assets/Asset", doc, XPathConstants.NODESET);
+
+			for (int i=0; i<assetsNodes.getLength(); i++) {
+				String name = xpath.evaluate("@relativePath", assetsNodes.item(i));
+				Vector2 normalizeCoeff = getNormalizeCoeff(combine(name));
+
+				NodeList shapesNodes = (NodeList)xpath.evaluate("Shape", assetsNodes.item(i), XPathConstants.NODESET);
+				ShapeModel[] shapes = new ShapeModel[shapesNodes.getLength()];
+				for (int j=0; j<shapesNodes.getLength(); j++) {
+					NodeList verticesNodes = (NodeList)xpath.evaluate("Vertex", shapesNodes.item(j), XPathConstants.NODESET);
+					Vector2[] vertices = new Vector2[verticesNodes.getLength()];
+					for (int k=0; k<verticesNodes.getLength(); k++) {
+						double x = (Double)((Double)xpath.evaluate("@x", verticesNodes.item(k), XPathConstants.NUMBER));
+						double y = (Double)xpath.evaluate("@y", verticesNodes.item(k), XPathConstants.NUMBER);
+						vertices[k] = new Vector2((float)x * normalizeCoeff.x, (float)y * normalizeCoeff.y);
+					}
+					shapes[j] = new ShapeModel(vertices);
+				}
+
+				NodeList polygonsNodes = (NodeList)xpath.evaluate("Polygon", assetsNodes.item(i), XPathConstants.NODESET);
+				PolygonModel[] polygons = new PolygonModel[polygonsNodes.getLength()];
+				for (int j=0; j<polygonsNodes.getLength(); j++) {
+					NodeList verticesNodes = (NodeList)xpath.evaluate("Vertex", polygonsNodes.item(j), XPathConstants.NODESET);
+					Vector2[] vertices = new Vector2[verticesNodes.getLength()];
+					for (int k=0; k<verticesNodes.getLength(); k++) {
+						double x = (Double)((Double)xpath.evaluate("@x", verticesNodes.item(k), XPathConstants.NUMBER));
+						double y = (Double)xpath.evaluate("@y", verticesNodes.item(k), XPathConstants.NUMBER);
+						vertices[k] = new Vector2((float)x * normalizeCoeff.x, (float)y * normalizeCoeff.y);
+					}
+					polygons[j] = new PolygonModel(vertices);
+				}
+
+				String path = IoManager.instance.combine(name);
+				AssetModel am = new AssetModel(path);
+				am.getShapes().addAll(Arrays.asList(shapes));
+				am.getPolygons().addAll(Arrays.asList(polygons));
+				AssetsManager.instance().getList().add(am);
+			}
+
+		} catch (SAXException ex) {
+		} catch (ParserConfigurationException ex) {
+		} catch (XPathExpressionException ex) {
+		}
+	}
+
+	private void importAsBinary() throws IOException {
 		DataInputStream is = new DataInputStream(new FileInputStream(outputFile));
 
 		while (is.available() > 0) {
 			String name = FilenameUtils.separatorsToSystem(is.readUTF());
-
 			Vector2 normalizeCoeff = getNormalizeCoeff(combine(name));
 
 			ShapeModel[] shapes = new ShapeModel[is.readInt()];
-			for (int i=0; i<shapes.length; i++)
-				shapes[i] = new ShapeModel(readVecs(is, normalizeCoeff));
+			for (int i=0; i<shapes.length; i++) {
+				Vector2[] vertices = new Vector2[is.readInt()];
+				for (int j=0; j<vertices.length; j++) {
+					vertices[j] = new Vector2();
+					vertices[j].x = is.readFloat() * normalizeCoeff.x;
+					vertices[j].y = is.readFloat() * normalizeCoeff.y;
+				}
+				shapes[i] = new ShapeModel(vertices);
+			}
 
 			PolygonModel[] polygons = new PolygonModel[is.readInt()];
-			for (int i=0; i<polygons.length; i++)
-				polygons[i] = new PolygonModel(readVecs(is, normalizeCoeff));
+			for (int i=0; i<polygons.length; i++) {
+				Vector2[] vertices = new Vector2[is.readInt()];
+				for (int j=0; j<vertices.length; j++) {
+					vertices[j] = new Vector2();
+					vertices[j].x = is.readFloat() * normalizeCoeff.x;
+					vertices[j].y = is.readFloat() * normalizeCoeff.y;
+				}
+				polygons[i] = new PolygonModel(vertices);
+			}
 
 			String path = IoManager.instance.combine(name);
 			AssetModel am = new AssetModel(path);
@@ -160,8 +324,10 @@ public class IoManager extends ChangeableObject {
 	}
 
 	// -------------------------------------------------------------------------
+	// Helpers
+	// -------------------------------------------------------------------------
 
-	public Vector2 getNormalizeCoeff(String assetPath) {
+	private Vector2 getNormalizeCoeff(String assetPath) {
 		FileHandle file = Gdx.files.absolute(assetPath);
 		if (!file.exists())
 			return new Vector2(1, 1);
@@ -170,33 +336,5 @@ public class IoManager extends ChangeableObject {
 		Vector2 coeff = new Vector2(pm.getWidth() / 100f, pm.getHeight() / 100f);
 		pm.dispose();
 		return coeff;
-	}
-	
-	// -------------------------------------------------------------------------
-
-	private void writeVec(DataOutputStream os, Vector2 v, Vector2 normalizeCoeff) throws IOException {
-		os.writeFloat(v.x / normalizeCoeff.x);
-		os.writeFloat(v.y / normalizeCoeff.y);
-	}
-
-	private void writeVecs(DataOutputStream os, Vector2[] vs, Vector2 normalizeCoeff) throws IOException {
-		os.writeInt(vs.length);
-		for (int i=0; i<vs.length; i++)
-			writeVec(os, vs[i], normalizeCoeff);
-	}
-
-	private Vector2 readVec(DataInputStream is, Vector2 normalizeCoeff) throws IOException {
-		Vector2 v = new Vector2();
-		v.x = is.readFloat() * normalizeCoeff.x;
-		v.y = is.readFloat() * normalizeCoeff.y;
-		return v;
-	}
-
-	private Vector2[] readVecs(DataInputStream is, Vector2 normalizeCoeff) throws IOException {
-		int len = is.readInt();
-		Vector2[] vs = new Vector2[len];
-		for (int i=0; i<len; i++)
-			vs[i] = readVec(is, normalizeCoeff);
-		return vs;
 	}
 }
