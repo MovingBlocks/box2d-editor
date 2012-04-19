bce9d1498988256fad414daeb6135f13bfd8dd7e
AurelienRibon <AurelienRibon@localhost>
2012-04-19 19:13:56 +0200

    + many changes once more

--- editor/src/aurelienribon/utils/notifications/AutoListModel.java
+++ editor/src/aurelienribon/utils/notifications/AutoListModel.java
@@ -1,5 +1,6 @@
 package aurelienribon.utils.notifications;
 
+import java.util.List;
 import javax.swing.ListModel;
 import javax.swing.event.EventListenerList;
 import javax.swing.event.ListDataEvent;
@@ -51,18 +52,6 @@ public class AutoListModel<T> implements ListModel<T> {
 	// Helpers
 	// -------------------------------------------------------------------------
 
-	private void fireIntervalAdded(int idx1, int idx2) {
-		ListDataEvent evt = new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, idx1, idx2);
-		for (ListDataListener listener : listeners.getListeners(ListDataListener.class))
-			listener.intervalAdded(evt);
-	}
-
-	private void fireIntervalRemoved(int idx1, int idx2) {
-		ListDataEvent evt = new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, idx1, idx2);
-		for (ListDataListener listener : listeners.getListeners(ListDataListener.class))
-			listener.intervalRemoved(evt);
-	}
-
 	private void fireContentsChanged(int idx1, int idx2) {
 		ListDataEvent evt = new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, idx1, idx2);
 		for (ListDataListener listener : listeners.getListeners(ListDataListener.class))
@@ -75,17 +64,10 @@ public class AutoListModel<T> implements ListModel<T> {
 
 	private final ObservableList.ListChangeListener<T> modelListener = new ObservableList.ListChangeListener<T>() {
 		@Override
-		public void elementAdded(Object source, int idx, T elem) {
-			fireIntervalAdded(idx, idx);
-			if (elem instanceof Changeable)
-				((Changeable)elem).addChangeListener(elemChangeListener);
-		}
-
-		@Override
-		public void elementRemoved(Object source, int idx, T elem) {
-			fireIntervalRemoved(idx, idx);
-			if (elem instanceof Changeable)
-				((Changeable)elem).removeChangeListener(elemChangeListener);
+		public void changed(Object source, List<T> added, List<T> removed) {
+			fireContentsChanged(0, model.size());
+			for (T elem : added) if (elem instanceof Changeable) ((Changeable)elem).addChangeListener(elemChangeListener);
+			for (T elem : removed) if (elem instanceof Changeable) ((Changeable)elem).removeChangeListener(elemChangeListener);
 		}
 	};
 
